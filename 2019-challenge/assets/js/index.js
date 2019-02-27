2ef35eefd322acf6cae5083ce303a92288976819
veronica <veronicadonoso1506@gmail.com>
2019-02-26 20:06:33 -0500

    Day15-100daysofcode

--- 2019-challenge/assets/js/index.js
+++ 2019-challenge/assets/js/index.js
@@ -181,3 +181,71 @@ function findDuplicates (arr) {
 }
 
 console.log(findDuplicates([2, 3, 4, 2, 5, 6, 4, 7, 8, 2, 6, 9]))
+
+/* Write a JavaScript program to compute the union of two arrays.
+Sample Data :
+console.log(union([1, 2, 3], [100, 2, 1, 10]));
+[1, 2, 3, 10, 100] */
+function unionArr (arr1, arr2) {
+  let object = {}
+  let result = []
+  let twoInOneArr = arr1.concat(arr2)
+
+  twoInOneArr.forEach(function (number) {
+    if (!object[number]) { object[number] = 0 };
+    object[number] += 1
+  })
+
+  for (let key in object) {
+    if (object[key] <= 2) {
+      result.push(key)
+    }
+  }
+  return result
+}
+console.log(unionArr([1, 2, 3], [100, 2, 1, 10]))
+// [1, 2, 3, 10, 100]
+
+Write a JavaScript function to find the difference of two arrays.
+Test Data :
+console.log(difference([1, 2, 3], [100, 2, 1, 10]));
+["3", "10", "100"]
+console.log(difference([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]));
+["6"]
+console.log(difference([1, 2, 3], [100, 2, 1, 10]));
+["3", "10", "100" */
+function difference (arr1, arr2) {
+  let twoInOneArr = arr1.concat(arr2)
+  let newArr = []
+  let object = {}
+  let result = []
+
+  flatten(twoInOneArr)
+  function flatten (twoInOneArr) {
+    twoInOneArr.forEach(number => {
+      if (!Array.isArray(number)) {
+        newArr.push(number)
+      } else {
+        flatten(number)
+      }
+    })
+  }
+
+  newArr.forEach(function (number) {
+    if (!object[number]) { object[number] = 0 };
+    object[number] += 1
+  })
+
+  for (let key in object) {
+    if (object[key] === 1) {
+      result.push(key)
+    }
+  }
+  return result
+}
+console.log(difference([1, 2, 3], [100, 2, 1, 10]))
+// ["3", "10", "100"]
+console.log(difference([1, 2, 3, 4, 5], [1, [2], [3, [[4]]], [5, 6]]))
+// ["6"]
+console.log(difference([1, 2, 3], [100, 2, 1, 10]))
+// ["3", "10", "100"
